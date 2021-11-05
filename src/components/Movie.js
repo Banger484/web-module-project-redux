1440d192bbb439c4a5632d30dd09417693920c2b
Bret Banger <Banger484@gmail.com>
2021-11-04 21:54:06 -0500

    finished working through add/remove/toggle favorites

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import { deleteMovie } from '../actions/movieActions';
+import { addFavorite } from '../actions/favoritesActions';
 
 const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const { movies, deleteMovie } = props;
+    const { movies, deleteMovie, displayFavorites, addFavorite } = props;
     const movie = movies.find(movie=>movie.id===Number(id));
 
+    const handleFavoriteClick = () => {
+        addFavorite(movie);
+    }
     const handleDeleteClick = () => {
         deleteMovie(movie.id);
         push('/movies')
@@ -44,7 +48,7 @@ const Movie = (props) => {
                         </section>
                         
                         <section>
-                            <span className="m-2 btn btn-dark">Favorite</span>
+                            {displayFavorites && <span onClick={handleFavoriteClick} className="m-2 btn btn-dark">Favorite</span>}
                             <span className="delete" onClick={handleDeleteClick}><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
@@ -55,7 +59,8 @@ const Movie = (props) => {
 }
 const mapStateToProps = (state) => {
     return ({
-        movies:state.movies
+        displayFavorites: state.favoritesReducer.displayFavorites,
+        movies:state.movieReducer.movies
     })
 }
-export default connect(mapStateToProps, {deleteMovie})(Movie);
+export default connect(mapStateToProps, {deleteMovie, addFavorite})(Movie);
